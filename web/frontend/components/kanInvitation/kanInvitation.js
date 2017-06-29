36a677a46b57cf261772032a424592cbae472204
Jean-Paul REMAN <jpolair@gmail.com>
2017-06-30 00:53:34 +0200

    change kanInvitation.js

---
+++ web/frontend/components/kanInvitation/kanInvitation.js
@@ -0,0 +1,22 @@
+'use strict';
+
+COMPNT
+  .component("kanInvitation", {
+
+    templateUrl: '/components/kanInvitation/kanInvitation.html',
+
+    bindings: {
+
+      users: '<',
+      newUser: '<'
+    },
+
+    controller: ['kanInvitationService', '$log', function (kanInvitationService, $log) {
+      this.$onInit = () => {
+        console.log(this.users); 
+      }
+      this.sendInvit = (newUser) => {
+          return kanInvitationService.postInvitation(newUser);
+        }
+    }]
+  });
