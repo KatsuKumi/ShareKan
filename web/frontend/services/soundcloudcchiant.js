8e5fdc080c988d2ac2f39274baa8258d09d38277
Piotr-V <pierrevicq@hotmail.fr>
2017-06-29 23:55:00 +0200

    Création de la page de vue de playlists

--- web/frontend/services/soundcloudcchiant.js
+++ web/frontend/services/soundcloudcchiant.js
@@ -1,11 +1,11 @@
 'use strict';
 
-SERVICES.service('kanListService', ['$http', '$log', '$q', function ($http, $log, $q) {
+SERVICES.service('soundcloudcchiant', ['$http', '$log', '$q', function ($http, $log, $q) {
 
-  this.getKanList = () => {
+  this.getSoundcloudId = (url) => {
     return $q((resolve, reject) => {
-      $http.get("http://localhost:8888/web/app_dev.php/api/playlist/get/all").then((response) => {
-        resolve(response.data);
+      $http.get("http://api.soundcloud.com/resolve.json?url="+ url +"/tracks&client_id=2t9loNQH90kzJcsFCODdigxfp325aq4z&app_version=1498753949").then((response) => {
+        resolve(response.data.id);
       })
         .catch((err) => {
           reject(err);
