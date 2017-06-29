4bb90ef095521bad7b8e214d4677bd49aeb5cc50
Jean-Paul REMAN <jpolair@gmail.com>
2017-06-30 00:47:13 +0200

    change file kanInvitatiService

--- web/frontend/services/kanInvitationService.js
+++ web/frontend/services/kanInvitationService.js
@@ -1,26 +1,38 @@
 'use strict';
 
-
-
 SERVICES.service('kanInvitationService', ['$http', '$log', '$q', function ($http, $log, $q) {
 
     this.getKanInvitationList = () => {
 
         var defer = $q.defer();
 
-        $http.get("http://localhost:8888/web/app_dev.php/api/playlist/get/all").then((response) => {
+        $http.get("http://localhost:3000/users").then((response) => {
+       // $http.get("http://localhost:8888/web/app_dev.php/api/user/invite").then((response) => {
             defer.resolve(response.data);
+            console.log(response.data);
         }).catch((err) => {
             $log.debug(`Error: ${err}`);
             defer.reject(err);
         });
-
         return defer.promise;
     };
-
-    this.getKanAcceptedInvitation = (id) => {
+  
+    this.getKanAcceptedInvitation = (email) => {
     return $q((resolve, reject) => {
-      $http.get("http://localhost:8888/web/app_dev.php/api/playlist/get/all").then((response) => {
+      $http.get("http://localhost:3000/users?email=" + email).then((response) => {
+      //$http.get("http://localhost:8888/web/app_dev.php/api/user/invite").then((response) => {
+        resolve(response.data);
+      })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  this.postInvitation = (newUser) => {
+    return $q((resolve, reject) => {
+      $http.post("http://localhost:3000/users" , newUser).then((response) => {
+      //$http.get("http://localhost:8888/web/app_dev.php/api/user/invite").then((response) => {
         resolve(response.data);
       })
         .catch((err) => {
