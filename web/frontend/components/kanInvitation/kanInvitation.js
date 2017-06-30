0e713882bb0ddcda5a5149e61392ced7974de60b
Jean-Paul REMAN <jpolair@gmail.com>
2017-06-30 02:15:31 +0200

    Clean master

--- web/frontend/components/kanInvitation/kanInvitation.js
+++ web/frontend/components/kanInvitation/kanInvitation.js
@@ -1,19 +1,48 @@
+
+
 'use strict';
 
-COMPNT
-  .component("kanInvitation", {
+SERVICES.service('kanInvitationService', ['$http', '$log', '$q', function ($http, $log, $q) {
 
-    templateUrl: '/components/kanInvitation/kanInvitation.html',
+    this.getKanInvitationList = () => {
 
-    bindings: {
-      kanInvited:'<'
-    },
+        var defer = $q.defer();
 
-    controller: ['kanInvitationService', '$log', function (kanInvitationService, $log) {
-      this.$onInit = () => {
-        kanInvitationService.getKanInvitationList();
-      }
+        $http.get("http://localhost:3000/users").then((response) => {
+       // $http.get("http://localhost:8888/web/app_dev.php/api/user/invite").then((response) => {
+            defer.resolve(response.data);
+            console.log(response.data);
+        }).catch((err) => {
+            $log.debug(`Error: ${err}`);
+            defer.reject(err);
+        });
+        return defer.promise;
+    };
+  
+    this.getKanAcceptedInvitation = (email) => {
+    return $q((resolve, reject) => {
+      $http.get("http://localhost:3000/users?email=" + email).then((response) => {
+      //$http.get("http://localhost:8888/web/app_dev.php/api/user/invite").then((response) => {
+        resolve(response.data);
+      })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
 
-      
-    }]
-  });
+  this.postInvitation = (newUser) => {
+     console.log(newUser);
+    return $q((resolve, reject) => {
+      $http.post("http://localhost:8888/web/app_dev.php/api/user/invite" , newUser).then((response) => {
+      //$http.get("http://localhost:8888/web/app_dev.php/api/user/invite").then((response) => {
+
+        resolve(response.data);
+      })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+}]);
